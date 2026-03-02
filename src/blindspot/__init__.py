7f4958a95bd2200c7d7c60b15f88fe5cec76c003
jenna-vc <jennavc@unc.edu>
2026-03-02 11:56:55 -0500

    Reupload git
    new stuff to fix and reupload

--- src/blindspot/__init__.py
+++
@@ -1,7 +0,0 @@
-from blindspot.functions.BlindSpot_blindingcode import run_blinder
-from blindspot.functions.BlindSpot_unblindingapp import run_unblinder
-
-
-def main():
-    from .BlindSpot_MainGUI import main as gui_main 
-    gui_main()
